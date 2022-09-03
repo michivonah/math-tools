b654d6bd5c9a171dde719bdfa5b9a2553e632817
michivonah <info@michivonah.ch>
2022-09-03 20:56:41 +0200

    add service worker #5

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const workerCache = "math-tools-app-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.json",
+  "/assets/calc.js",
+  "/assets/functions.js",
+  "/assets/images/logo.svg",
+  "/assets/images/logo_96.ico",
+  "/assets/images/logo_96.png",
+  "/assets/images/logo_256.png",
+  "/assets/images/logo_512.png",
+  "/assets/images/logo_720.png",
+  "/assets/images/logo_1024.png",
+  "https://unpkg.com/akar-icons-fonts",
+  "https://unpkg.com/aos@2.3.1/dist/aos.js",
+  "https://unpkg.com/aos@2.3.1/dist/aos.css",
+  "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(workerCache).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
