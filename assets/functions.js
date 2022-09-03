dcf238e86a75ae78cf10cd312f398702d81d95fa
michivonah <info@michivonah.ch>
2022-09-03 16:14:36 +0200

    add functions.js

---
+++ assets/functions.js
@@ -0,0 +1,36 @@
+// functions.js
+// Michi von Ah - 2022
+
+function toggleSection(sectionName, type){
+    var section = document.getElementById(sectionName);
+    if(section.style.display == "block" || section.style.display == "flex"){
+      section.style.display = "none";
+    }
+    else{
+      section.style.display = type;
+    }
+  }
+
+
+  function toggleClass(className){
+    var elements = document.getElementsByClassName(className);
+    for(var i = 0; i < elements.length; i++){
+      if(elements[i].style.display == "block"){
+        elements[i].style.display = "none";
+      }
+      else{
+        elements[i].style.display = "block";
+      }
+    }
+  }
+
+  function changeTool(){
+    var allTools = document.getElementsByClassName('tool');
+    for(var i = 0; i < allTools.length; i++){
+      var current = allTools[i].id;
+      var toolId = allTools[i].dataset.toolid;
+      var state = document.getElementById(toolId).checked;
+      if(state == true) document.getElementById(current).style.display = "block";
+      else document.getElementById(current).style.display = "none";
+    }
+  }
