d5907cb36e1fbf7e9342cf9cf3bb1c21edc979c4
michivonah <info@michivonah.ch>
2022-09-11 11:31:47 +0200

    updated assets in service worker #5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const workerCache = "math-tools-app-v1-0-1"
+const workerCache = "math-tools-app-v1-0-2"
 const assets = [
   "/",
   "/index.html",
@@ -13,6 +13,8 @@ const assets = [
   "/assets/images/logo_512.png",
   "/assets/images/logo_720.png",
   "/assets/images/logo_1024.png",
+  "/assets/images/math_background.svg",
+  "/assets/images/phytagoras.svg",
   "https://unpkg.com/akar-icons-fonts",
   "https://unpkg.com/aos@2.3.1/dist/aos.js",
   "https://unpkg.com/aos@2.3.1/dist/aos.css",
